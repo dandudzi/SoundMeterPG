f6391d06eca8b3b214b6f66af32e3af3d5358082
filgierl <filgierl@student.pg.gda.pl>
2016-08-29 22:02:58 +0200

    Nie ruszać!

--- app/src/main/java/pl/gda/pg/eti/kask/soundmeterpg/Fragments/Measurements.java
+++ app/src/main/java/pl/gda/pg/eti/kask/soundmeterpg/Fragments/Measurements.java
@@ -15,6 +15,6 @@ public class Measurements extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.Measurements, container, false);
+        return inflater.inflate(R.layout.measurements, container, false);
     }
 }
