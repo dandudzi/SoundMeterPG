3a88157796f46f896c1cb8a1ea3f865da76906e1
dandudzi <dandudzi@student.pg.gda.pl>
2016-09-13 18:00:15 +0200

    poprawiony drawer :)  i tetsy do niego łatwiej teraz wprowadzac zmiany :)

--- app/src/main/java/pl/gda/pg/eti/kask/soundmeterpg/Exceptions/EndTaskException.java
+++ app/src/main/java/pl/gda/pg/eti/kask/soundmeterpg/Exceptions/EndTaskException.java
@@ -3,5 +3,5 @@ package pl.gda.pg.eti.kask.soundmeterpg.Exceptions;
 /**
  * Created by Daniel on 13.09.2016 at 17:55 :).
  */
-public class EndTask extends Exception {
+public class EndTaskException extends Exception {
 }
